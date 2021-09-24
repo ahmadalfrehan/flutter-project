0450315c38dde4f886dd3731ae14e1828d1c7897
ahmad alfrehan <81897367+ahmadfrehan5@users.noreply.github.com>
2021-09-24 01:56:08 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -224,3 +224,122 @@ class _MyHomepageState extends State<MyHomePage> {
     );
   }
 }
+/*
+child: FutureBuilder(
+          future: fetchPhotos(),
+          builder: (ctx, AsyncSnapshot) {
+            if (AsyncSnapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else {
+              return ListView.builder(
+                itemBuilder: (_, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        AsyncSnapshot.hasData
+                            ? AsyncSnapshot.data[index]['thumbnailUrl']
+                            : Text("error"),
+                      ),
+                    ),
+                    title: AsyncSnapshot.hasData
+                        ? Text(AsyncSnapshot.data[index]['title'])
+                        : Text("error"),
+                    subtitle: AsyncSnapshot.hasData
+                        ? Text("${AsyncSnapshot.data[index]['id']}")
+                        : Text("error"),
+                  );
+                },
+              );
+            }
+          },
+        
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}*/
+/**/
